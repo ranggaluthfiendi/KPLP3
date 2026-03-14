4557f3d188cacb0188f6be725e29c8a1fda3eb49
Rang <echo.rangrang@gmail.com>
2026-03-14 14:18:04 +0700

    Menambahkan tampilan GUI

--- Modul3_103022400021/Form1.Designer.cs
+++ Modul3_103022400021/Form1.Designer.cs
@@ -28,19 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label3 = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(316, 198);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Satuan Awal";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(316, 363);
+            label2.Name = "label2";
+            label2.Size = new Size(112, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Satuan Akhir";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Celcius", "Fahrenheit", "Kelvin", "Reamur" });
+            comboBox1.Location = new Point(316, 239);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(112, 33);
+            comboBox1.TabIndex = 2;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Items.AddRange(new object[] { "Celcius", "Fahrenheit", "Kelvin", "Reamur" });
+            comboBox2.Location = new Point(316, 403);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(112, 33);
+            comboBox2.TabIndex = 3;
+            comboBox2.SelectedIndexChanged += comboBox2_SelectedIndexChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(519, 198);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 25);
+            label3.TabIndex = 4;
+            label3.Text = "Nilai Awal";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(519, 363);
+            label4.Name = "label4";
+            label4.Size = new Size(92, 25);
+            label4.TabIndex = 5;
+            label4.Text = "Nilai Akhir";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(519, 241);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(367, 31);
+            textBox1.TabIndex = 6;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(519, 405);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(367, 31);
+            textBox2.TabIndex = 7;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(966, 239);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 8;
+            button1.Text = "Convert";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1385, 742);
+            Controls.Add(button1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button1;
     }
 }
--- Modul3_103022400021/Form1.cs
+++ Modul3_103022400021/Form1.cs
@@ -9,6 +9,39 @@ namespace Modul3_103022400021
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            comboBox1.Items.Add("Celcius");
+            comboBox1.Items.Add("Fahreinheit");
+            comboBox1.Items.Add("Kelvin");
+            comboBox1.Items.Add("Reamur");
+
+            comboBox2.Items.Add("Celcius");
+            comboBox2.Items.Add("Fahreinheit");
+            comboBox2.Items.Add("Kelvin");
+            comboBox2.Items.Add("Reamur");
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
 
         }
     }
